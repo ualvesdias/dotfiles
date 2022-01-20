d53c79a3fdf2b690901857b898bbc39683417bb8
Ulisses Alves <ulisses.alves@protonmtail.com>
2022-01-20 19:36:26 -0300

    update dotfiles

--- .local/bin/randompassword.sh
+++ .local/bin/randompassword.sh
@@ -2,3 +2,6 @@
 
 amount=$(rofi -dmenu) && tr -dc 'A-Za-z0-9!@#$%&*(){}[],.;:?~' < /dev/urandom | dd bs=1 count=${amount} 2>/dev/null| xclip -selection clipboard || /usr/bin/rofi -e "No number provided."
 
+sleep 10
+printf '' | xclip -selection clipboard
+
